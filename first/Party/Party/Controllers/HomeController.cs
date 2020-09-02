72cf832b821f7cdbfb116a2a1a58b3a725b0eec8
RomanPolishukov <roman.polishukov@yandex.ru>
2020-09-02 12:16:42 +0300

    first lessons

---
+++ first/Party/Party/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Party.Models;
+
+namespace Party.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            string time = DateTime.Now.ToString();
+            ViewBag.Greeting = hour < 12 ? "Доброе утро" : "Добрый вечер";
+            ViewBag.Timer = time;
+            return View();
+        }
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                // TODO: Email response to the party organizer
+                return View("Thanks", guestResponse);
+            }
+            else
+            {
+                // there is a validation error
+                return View();
+            }
+        }
+
+    }
+}
---
+++ first/Party/Party/Models/GuestResponse.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Party.Models
+{
+    public class GuestResponse
+    {
+        [Required(ErrorMessage = "Введите имя!!!")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Введите почту!!!")]
+        [RegularExpression(@".+\@.+\..+", ErrorMessage = "Почта введена не верно")]
+        public string Email { get; set; }
+        
+        [Required(ErrorMessage = "Введите ноиер телефона!!!")]
+        public string Phone { get; set; }
+        
+        [Required(ErrorMessage = "Пожалуйста, уточните, будете ли вы присутствовать")]
+        public bool? WillAttend { get; set; }
+    }
+}
